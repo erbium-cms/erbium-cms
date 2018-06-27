e3114d78ee60340474726c6b05800b4d8597f5dd
Walter Stocker <walter.stocker@siemens-healthineers.com>
2018-06-27 14:59:58 +0200

    feat: new script to create markdown blog entries from docx with pandoc

---
+++ scripts/doc2md.js
@@ -0,0 +1,62 @@
+const { readdir, ensureDir, writeFile } = require('fs-extra')
+const util = require('util')
+const execFile = util.promisify(require('child_process').execFile)
+const path = require('path')
+const uuidv4 = require('uuid/v4')
+
+// pandoc --extract-media=assets -s word.docx -t markdown -o markdown.md
+function execPandoc(srcDir, distDir, srcFile, outFile) {
+  const pandoc = path.join(process.env.LOCALAPPDATA, 'Pandoc/pandoc')
+
+  const args = [
+    `--extract-media=${distDir}\\assets`,
+    '-s',
+    path.join(srcDir, srcFile),
+    '-t',
+    'markdown',
+    '-o',
+    path.join(distDir, outFile)
+  ]
+
+  execFile(pandoc, args)
+    .then(result => console.log(result.stdout))
+    .catch(err => console.error(err))
+}
+
+const srcDir = 'D:\\temp\\blog-posts-export'
+const distDir = 'D:\\temp\\blog-posts-export-md'
+
+function convertEntry(name, blogEntries) {
+  const uuid = uuidv4()
+  const filename = name.replace(/^(.*)\.docx$/, (match, f) => f)
+  const entry = {
+    id: uuid,
+    title: filename,
+    author: '',
+    modified: '',
+    content: `${uuid}/${filename}.md`
+  }
+
+  ensureDir(`${distDir}\\${uuid}`)
+  ensureDir(`${distDir}\\${uuid}\\assets`)
+  execPandoc(srcDir, `${distDir}\\${uuid}`, name, `${filename}.md`)
+
+  blogEntries.push(entry)
+}
+
+readdir(srcDir)
+  .then(contents => {
+    let blogEntries = []
+
+    contents
+      .filter(f => /^.*\.docx$/.test(f))
+      .forEach(f => convertEntry(f, blogEntries))
+
+    return blogEntries
+  })
+  .then(blogEntries => {
+    let json = JSON.stringify(blogEntries)
+    writeFile(`${distDir}/blog-entries.json`, json, 'utf8').then(() =>
+      console.log('done')
+    )
+  })
